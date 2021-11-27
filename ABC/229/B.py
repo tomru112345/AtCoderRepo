a88216aa075b55cdfbe23be7d460940a7c22c4d1
tomru112345 <kazt23@gmail.com>
2021-11-27 23:15:16 +0900

    fix 229

--- ABC/229/B.py
+++ ABC/229/B.py
@@ -1,26 +1,18 @@
 A, B = map(int, input().split())
 
-A_s = str(A)
-B_s = str(B)
+A_s = str(A)[::-1]
+B_s = str(B)[::-1]
+
 min_len = min(len(A_s), len(B_s))
-max_len = max(len(A_s), len(B_s))
 
-A_list = [0] * len(A_s)
-B_list = [0] * len(B_s)
-
-num = 0
-
-for i in reversed(range(len(A_s))):
-    A_list[(len(A_s) - 1) - i] = int(A_s[i])
-
-for i in reversed(range(len(B_s))):
-    B_list[(len(B_s) - 1) - i] = int(B_s[i])
+flag = False
 
 for i in range(min_len):
-    if (A_list[i] + B_list[i] > 9):
-        num += 1
-        if (num == 1):
+    if (int(A_s[i]) + int(B_s[i]) > 9):
+        flag = True
+        if (flag):
             print("Hard")
+            break
 
-if (num == 0):
+if (not flag):
     print("Easy")
