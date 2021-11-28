d18ae5273aa28cca31fd07e25b52a078b96fff04
tomru112345 <kazt23@gmail.com>
2021-11-28 15:15:08 +0900

    fix a

--- ABC/229/A.py
+++ ABC/229/A.py
@@ -1,29 +1,9 @@
 S1 = input()
 S2 = input()
 
-map_s = [[S1[0], S1[1]], [S2[0], S2[1]]]
-
-b = [False] * 4
-
-if (S1[0] == "#" and S1[1] == "#"):
-    b[0] = True
-if (S1[0] == "#" and S2[0] == "#"):
-    b[1] = True
-if (S2[0] == "#" and S2[1] == "#"):
-    b[2] = True
-if (S1[1] == "#" and S2[1] == "#"):
-    b[3] = True
-
-if (S1[0] == "#" and S2[1] == "#") and (S1[0] == "." and S2[1] == "."):
+if (S1 == "#." and S2 == ".#") or (S1 == ".#" and S2 == "#."):
     print("No")
-elif (S1[1] == "#" and S2[0] == "#") and (S1[1] == "." and S2[0] == "."):
-    print("No")
+    exit()
 else:
-    num = 0
-    for i in b:
-        if (i):
-            num += 1
-            if (num == 1):
-                print("Yes")
-    if (num == 0):
-        print("No")
+    print("Yes")
+    exit()
